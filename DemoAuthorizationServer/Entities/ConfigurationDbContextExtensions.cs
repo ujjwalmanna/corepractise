1290704b80507923c5dc156855645e4bd0a7185e
Ujjwal_Manna <Ujjwal_Manna@Dell.com>
2019-04-30 16:29:16 +0530

    Amended code to add persister storage

---
+++ DemoAuthorizationServer/Entities/ConfigurationDbContextExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoAuthorizationServer.Entities;
+using DemoAuthorizationServer.Models;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+
+namespace DemoAuthorizationServer.Entities
+{
+    public static class ConfigurationDbContextExtensions
+    {
+        public static void EnsureSeedDataForContext(this ConfigurationDbContext context)
+        {
+            // Add 2 demo users if there aren't any users yet
+            if (!context.Clients.Any())
+            {
+                foreach (var client in Config.GetClients())
+                {
+                    context.Clients.Add(client.ToEntity());
+                }
+
+                context.SaveChanges();
+            }
+
+            if (!context.IdentityResources.Any())
+            {
+                foreach (var resource in Config.GetIdentityResource())
+                {
+                    context.IdentityResources.Add(resource.ToEntity());
+                }
+
+                context.SaveChanges();
+            }
+
+            if (!context.ApiResources.Any())
+            {
+                foreach (var resource in Config.GetApiResources())
+                {
+                    context.ApiResources.Add(resource.ToEntity());
+                }
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- DemoAuthorizationServer/Startup.cs
+++ DemoAuthorizationServer/Startup.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using DemoAuthorizationServer.Entities;
 using DemoAuthorizationServer.Extensions;
 using DemoAuthorizationServer.Models;
 using DemoAuthorizationServer.Services;
+using IdentityServer4.EntityFramework.DbContexts;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -33,6 +36,7 @@ namespace DemoAuthorizationServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
             services.Configure<IISOptions>(iis =>
             {
                 iis.AuthenticationDisplayName = "Windows";
@@ -41,15 +45,46 @@ namespace DemoAuthorizationServer
             services.AddDbContext<EFUserContext>(db => db.UseSqlServer(Configuration.GetConnectionString("IDPUserDB")));
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddIdentityServer()
-                .AddDeveloperSigningCredential()
+                .AddSigningCredential(LoadCertificateFromStore()) //.AddDeveloperSigningCredential()
                 .AddUserStore() // replaced by .AddTestUsers(Config.GetUsers())
-                .AddInMemoryIdentityResources(Config.GetIdentityResource())
-                .AddInMemoryApiResources(Config.GetApiResources())
-                .AddInMemoryClients(Config.GetClients());
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = builder => builder.UseSqlServer(
+                        Configuration.GetConnectionString("IDPServerDB"),
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = builder => builder.UseSqlServer(
+                        Configuration.GetConnectionString("IDPServerDB"),
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                });
+            //.AddInMemoryIdentityResources(Config.GetIdentityResource())
+            //.AddInMemoryApiResources(Config.GetApiResources())
+            //.AddInMemoryClients(Config.GetClients());
+        }
+
+        /// <summary>
+        /// Command to use create certificate:
+        /// makecert -r -pe -n "CN=IDPSigningCert" -b 01/01/2019 -e 01/01/2020 -eku 1.3.6.1.5.5.7.3.3 -sky signature -a sha256 -len 2048 -ss my -sr LocalMachine
+        /// </summary>
+        /// <returns></returns>
+        public X509Certificate2 LoadCertificateFromStore()
+        {
+            string thumbprint = "c62a70e681b01a65e702d27fb39af785f2ef6920";
+            using (var store = new X509Store(StoreName.My, StoreLocation.LocalMachine))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                var certCollection = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, true);
+                if (certCollection.Count == 0)
+                    throw new Exception("The specified certificate wasn't found");
+                return certCollection[0];
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env,EFUserContext userContext)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,EFUserContext userContext,ConfigurationDbContext configContext,
+            PersistedGrantDbContext persistedGrantDbContext)
         {
             if (env.IsDevelopment())
             {
@@ -59,6 +94,12 @@ namespace DemoAuthorizationServer
             {
                 app.UseHsts();
             }
+
+            configContext.Database.Migrate();
+            configContext.EnsureSeedDataForContext();
+
+            persistedGrantDbContext.Database.Migrate();
+
             userContext.Database.Migrate();
             userContext.EnsureSeedDataForContext();
             app.UseIdentityServer();
