3f2ca5b6c2057a46be0987bec6c666fe1420a8f6
Ujjwal_Manna <Ujjwal_Manna@Dell.com>
2019-06-01 19:06:23 +0530

    Checked in

---
+++ TagHelperDemo/Controllers/SpeakerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TagHelperDemo.Controllers
+{
+    public class SpeakerController : Controller
+    {
+        public List<SpeakerData> Speakers = new List<SpeakerData>
+        {
+            new SpeakerData {SpeakerId = 10},
+            new SpeakerData {SpeakerId = 11},
+            new SpeakerData {SpeakerId = 12}
+        };
+
+        public int SpeakerId;
+
+        [Route("Speaker/{id}",Name="speakerdetail")]
+        public IActionResult Detail(int id)
+        {
+            SpeakerId = id;
+            return View(Speakers.
+                FirstOrDefault(a => a.SpeakerId == id));
+        }
+
+        
+        [Route("/Speaker/Evaluations", Name = "speakerevals")]
+        public IActionResult Evaluations()
+        {
+            return View();
+        }
+
+        
+
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(Speakers);
+        }
+    }
+
+    public class SpeakerData
+    {
+        public int SpeakerId { get; set; }
+    }
+}
